fefff900e4b29b94e868187f828f69117e04ce65
Luis Felipe <felipebarbosa22@hotmail.com>
2023-07-27 19:29:25 -0300

    Finished

--- curriculum/src/App.js
+++ curriculum/src/App.js
@@ -9,25 +9,54 @@ import Additional from './components/additional/additional';
 import Contato from './components/contato/contato';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css'; 
-
-
+import { Helmet } from 'react-helmet';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
+import './transitions.css'
 
 
 function App() {
+  /*Define o titulo na aba usando React Helmet*/
+  const TITLE = 'Currículo';
+  
+
   return (
+    <>
+    <Helmet>
+    <title>{TITLE}</title>
+    </Helmet>
+
     <Router>
     <NavBar />
-    <Switch>
-      <Route exact path="/">
+
+    
+      <Route render={({ location }) => (
+            <TransitionGroup>
+              <CSSTransition
+                key={location.key}
+                timeout={600}/*No maximo 0,5 segundos de animação*/
+                classNames="fade" >
+                
+                <Switch location={location}>
+
+
+                <Route exact path="/">
         <Banner />
         <Cards />
         <Additional />
       </Route>
+
+
       <Route path="/contato">
         <Contato />
       </Route>
     </Switch>
+    </CSSTransition>
+  </TransitionGroup>
+  )}
+  />
   </Router>
+
+  </>
   );
 }
 
--- curriculum/src/components/contato/contato.js
+++ curriculum/src/components/contato/contato.js
@@ -1,11 +1,49 @@
 
 import styles from './contato.module.css'
 
+import { Helmet } from 'react-helmet';
+
 export default function Contato()
 {
+    const TITLE = 'Entre em contato';
+    
     return(
-        <div>
-            <h1>Contato</h1>
+        <>
+        <Helmet>
+    <title>{TITLE}</title>
+    </Helmet>
+        <div className={styles.container}>
+            <div className={styles.estiloForm}>
+
+            <h2>Deseja entrar em contato? Preencha os campos</h2>
+            <form method="post" action="https://formspree.io/f/mrgvwylp">
+            <div>
+            <label >Nome:</label>
+            </div>
+            <input placeholder="Digite seu nome" />
+            
+            <div>
+            <label>Seu E-mail:</label>
+            </div>
+
+            <input type="email" id="email" placeholder="emailexemplo@outlook.com"/>
+            
+
+            <div>
+            <label>Sua mensagem:</label>
+            </div>
+
+            <textarea rows="8" maxlength="3000" id="texto" placeholder="Qual sua mensagem?" />
+
+            <div className={styles.divbotaoEnviar}>
+            <button type="submit" className={styles.botaoEnviar}>Enviar</button>
+            </div>
+
+        </form>
+
         </div>
+        
+        </div>
+        </>
     )
 }
