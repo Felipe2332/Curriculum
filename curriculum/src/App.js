c0d7a0739cdecfbddf9f3b3e7a076962b2a1783f
Luis Felipe <felipebarbosa22@hotmail.com>
2023-07-28 18:51:09 -0300

    Update App.js

--- curriculum/src/App.js
+++ curriculum/src/App.js
@@ -1,8 +1,5 @@
-import logo from './logo.svg';
-import styles from'./App.css';
+
 import Banner from './components/banner/banner';
-import Body from './components/body/body';
-import foto from './components/imagens/fotoMinha.png';
 import NavBar from './components/navBar/navBar';
 import Cards from './components/cards/cards';
 import Additional from './components/additional/additional';
@@ -17,47 +14,37 @@ import './transitions.css'
 function App() {
   /*Define o titulo na aba usando React Helmet*/
   const TITLE = 'Currículo';
-  
 
   return (
-    <>
-    <Helmet>
-    <title>{TITLE}</title>
-    </Helmet>
-
     <Router>
-    <NavBar />
+      
+      <Helmet>
+        <title>{TITLE}</title>
+      </Helmet>
 
-    
-      <Route render={({ location }) => (
-            <TransitionGroup>
-              <CSSTransition
-                key={location.key}
-                timeout={600}/*No maximo 0,5 segundos de animação*/
-                classNames="fade" >
-                
-                <Switch location={location}>
-
-
+      <NavBar />
+      
+      <Route
+        render={({ location }) => (
+          <TransitionGroup>
+            <CSSTransition key={location.key} timeout={400} classNames="fade">
+              <Switch location={location}>
                 <Route exact path="/">
-        <Banner />
-        <Cards />
-        <Additional />
-      </Route>
+                  {/* Componentes da página inicial */}
+                  <Banner />
+                  <Cards />
+                  <Additional />
+                </Route>
 
-
-      <Route path="/contato">
-        <Contato />
-      </Route>
-    </Switch>
-    </CSSTransition>
-  </TransitionGroup>
-  )}
-  />
-  </Router>
-
-  </>
+                <Route path="/contato">
+                  <Contato />
+                </Route>
+              </Switch>
+            </CSSTransition>
+          </TransitionGroup>
+        )}
+      />
+    </Router>
   );
 }
-
 export default App;
