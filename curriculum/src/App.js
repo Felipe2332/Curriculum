e9bee07120b94dda38e561a280b68d4a93212ece
Luis Felipe <felipebarbosa22@hotmail.com>
2023-07-31 19:15:29 -0300

    Fisished

--- curriculum/src/App.js
+++ curriculum/src/App.js
@@ -1,33 +1,36 @@
-
-import Banner from './components/banner/banner';
+import React, { useEffect, useState } from 'react';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
+import { Helmet } from 'react-helmet';
 import NavBar from './components/navBar/navBar';
+import Banner from './components/banner/banner';
 import Cards from './components/cards/cards';
 import Additional from './components/additional/additional';
 import Contato from './components/contato/contato';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css'; 
-import { Helmet } from 'react-helmet';
-import { TransitionGroup, CSSTransition } from 'react-transition-group';
-import './transitions.css'
-
+import './transitions.css';
 
 function App() {
-  /*Define o titulo na aba usando React Helmet*/
+  /* Define o titulo na aba usando React Helmet */
   const TITLE = 'Currículo';
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    setIsLoaded(true);
+  }, []);
 
   return (
     <Router>
-      
       <Helmet>
         <title>{TITLE}</title>
       </Helmet>
 
       <NavBar />
-      
+
       <Route
         render={({ location }) => (
           <TransitionGroup>
-            <CSSTransition key={location.key} timeout={400} classNames="fade">
+            <CSSTransition key={isLoaded ? location.key : 'main'} timeout={400} classNames="fade">
               <Switch location={location}>
                 <Route exact path="/">
                   {/* Componentes da página inicial */}
@@ -39,6 +42,9 @@ function App() {
                 <Route path="/contato">
                   <Contato />
                 </Route>
+
+                {/* Redirecionar para a página inicial. Sem isso, a pagina inicialmente não carrega */}
+                <Redirect to="/" />
               </Switch>
             </CSSTransition>
           </TransitionGroup>
@@ -47,4 +53,5 @@ function App() {
     </Router>
   );
 }
+
 export default App;
